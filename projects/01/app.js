b2445b63cb2ef7004f0b086a7411f2a7803b5a94
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-04-19 19:22:40 -0300

    Finished project#1

--- projects/01/app.js
+++ projects/01/app.js
@@ -5,7 +5,8 @@ let vm = new Vue({
         gameOver: false,
         winner: null,
         playerLife: 1,
-        monsterLife: 1
+        monsterLife: 1,
+        attacks: []
     },
     computed: {
         playerLifeBar() {
@@ -28,19 +29,77 @@ let vm = new Vue({
         }
     },
     methods: {
-        atack() {},
-        specialAtack() {},
-        damagePlayer() {
-            this.playerLife -= Math.random() / 2;
+        attack() {
+            this.damageMonster();
+            this.damagePlayer();
         },
-        damageMonster() {
-            //  ...
+        specialAttack() {
+            this.damageMonster(true);
+            this.damagePlayer(true);
+        },
+        damagePlayer(special) {
+            factor = special ? 15 : 10;
+
+            let damage = Math.random() / factor;
+            if (damage > this.playerLife) {
+                this.playerLife = 0;
+                this.gameIsOver("Monster");
+            } else {
+                this.playerLife -= damage;
+            }
+            this.attacks.push({
+                who: "player",
+                action: "perdeu",
+                value: damage * 100
+            });
+        },
+        damageMonster(special) {
+            factor = special ? 10 : 15;
+
+            let damage = Math.random() / factor;
+            if (damage > this.monsterLife) {
+                this.monsterLife = 0;
+                this.gameIsOver("Player");
+            } else {
+                this.monsterLife -= damage;
+            }
+            // this.attacks.push(`Monstro perdeu ${(damage * 100).toFixed(0)}`);
+            this.attacks.push({
+                who: "monster",
+                action: "perdeu",
+                value: damage * 100
+            });
         },
         healPlayer() {
-            // ...
+            this.damagePlayer();
+            let heal = Math.random() / 10;
+            if (this.playerLife + heal >= 1) {
+                this.playerLife = 1;
+            } else {
+                this.playerLife += heal;
+            }
+            // this.attacks.push(`Player ganhou ${(heal * 100).toFixed(0)}`);
+            this.attacks.push({
+                who: "player",
+                action: "ganhou",
+                value: heal * 100
+            });
         },
         giveUp() {
-            // ...
+            this.gameIsOver("Monster");
+        },
+        reset() {
+            this.gameOver = false;
+            this.playerLife = 1;
+            this.monsterLife = 1;
+            this.winner = null;
+            this.running = true;
+            this.attacks = [];
+        },
+        gameIsOver(winner) {
+            this.winner = winner;
+            this.gameOver = true;
+            this.running = false;
         }
     }
 });
