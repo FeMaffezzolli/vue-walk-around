38ec6bdd55b7b8045cd90f7e1443cfb79c9a5d06
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-04-20 14:34:23 -0300

    using cli

---
+++ basics/componente-intro/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Contadores</h1>
+    <p>{{contador}}</p>
+    <button @click="contador++">Mais</button>
+    <button @click="contador--">Menos</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  }
+};
+</script>
+
+<style>
+button {
+  background: #333;
+  color: #fff;
+}
+</style>
