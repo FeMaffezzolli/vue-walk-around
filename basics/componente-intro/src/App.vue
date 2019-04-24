3d217774573b69586ec622aacd8fbf1590d7b70f
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-04-23 22:48:05 -0300

    counter

--- basics/componente-intro/src/App.vue
+++ basics/componente-intro/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <h1>Contadores</h1>
-    <p>{{contador}}</p>
-    <button @click="contador++">Mais</button>
-    <button @click="contador--">Menos</button>
+    <contador/>
+    <contador/>
+    <contador/>
+    <hr>
+    <label for="ice-cream-choice">Choose a flavor:</label>
+    <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice">
+    <datalist id="ice-cream-flavors">
+      <option value="Chocolate"></option>
+      <option value="Coconut"></option>
+      <option value="Mint"></option>
+      <option value="Strawberry"></option>
+      <option value="Vanilla"></option>
+    </datalist>
   </div>
 </template>
 
@@ -21,5 +31,8 @@ export default {
 button {
   background: #333;
   color: #fff;
+}
+label {
+  font-family: "Fira Sans", sans-serif;
 }
 </style>
--- basics/componente-intro/src/main.js
+++ basics/componente-intro/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Contador from "./components/Contador";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component("contador", Contador);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount("#app");
