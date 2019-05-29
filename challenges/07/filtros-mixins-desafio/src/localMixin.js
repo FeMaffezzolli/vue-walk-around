21e64693fe82cfb62817554d7acdef9f3cf9a8b1
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-05-28 23:54:15 -0300

    Clever change

--- challenges/07/filtros-mixins-desafio/src/localMixin.js
+++ challenges/07/filtros-mixins-desafio/src/localMixin.js
@@ -7,14 +7,10 @@ export default {
         cInput2() {
             let str = this.inputC2;
             if (str && str.length > 0) {
-                let words = str.split(" ");
-                let counts = words.map(s => `(${s.length})`);
-                let finalArray = [];
-                for (let index = 0; index < counts.length; index++) {
-                    finalArray.push(words[index]);
-                    finalArray.push(counts[index]);
-                }
-                return finalArray.join(" ");
+                return str
+                    .split(" ")
+                    .map(s => `${s} (${s.length})`)
+                    .join(" ");
             }
             return "";
         }
